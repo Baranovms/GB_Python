5ddefee430a399b4a12e271429323e6ca5570baf
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2022-12-26 23:38:55 +0300

    Обновление дз4

--- HW_4/Task5.py
+++ HW_4/Task5.py
@@ -7,18 +7,11 @@ file3 = open('zadanie5_summa.txt', 'w')
 f1 = file1.readline()
 lst_f1 = f1.split()
 print(lst_f1, type(f1))
-# dict_f1 = {}
-# for i in range(len(lst_f1)):
-#     dict_f1[i] = lst_f1[i]
-# print(dict_f1, type(f1))
 
 f2 = file2.readline()
 lst_f2 = f2.split()
 print(lst_f2, type(f2))
-# dict_f2 = {}
-# for i in range(len(lst_f2)):
-#     dict_f2[i] = lst_f2[i]
-# print(dict_f2, type(f2))
+
 for i in range(len(lst_f1)):
     if lst_f1[i] != lst_f2[i]:
         file3.write(f'{str(int(lst_f1[i])+int(lst_f2[i]))} ')
