420fe68ee6cea985a91bad0719b3a0241c1b30a4
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2023-01-29 21:08:31 +0300

    Добавлена папка с ДЗ_8

--- HW_8_Class_book/controller.py
+++ HW_8_Class_book/controller.py
@@ -6,15 +6,13 @@ disciplines = []
 
 def start():
     while True:
-        print(base)
+
         pos_menu = view.menu()
         if pos_menu == 1:
             student = view.add_student()
             if student not in students:
                 students.append(student)
                 base[student] = {}
-                for discipline in students:
-                    base[student][discipline] = []
         if pos_menu == 2:
             discipline = view.add_discipline()
             if discipline not in disciplines:
@@ -23,7 +21,10 @@ def start():
                     base[student][discipline] = []
         if pos_menu == 3:
             student, discipline, assessment = view.get_assessment()
-            base[student][discipline].append(assessment)
+            if discipline not in disciplines or student not in students:
+                print('Not Found.\n Repeat, please')
+            else:
+                base[student][discipline].append(assessment)
         if pos_menu == 4:
             print(base)
         if pos_menu == 5:
@@ -31,3 +32,6 @@ def start():
             print(base[student])
         if pos_menu == 6:
             break
+        print('*' * 50)
+        print(base)
+        print('*' * 50)
